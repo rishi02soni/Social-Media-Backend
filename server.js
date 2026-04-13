486efcb5517e43a02d95cb1b42b9c19972ca744e
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-13 23:36:39 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log('Server running')
+);
